627f3375e6079b58bedcecaeaedb1c2a01d1e26c
OlekZak <olek7111137@gmail.com>
2025-09-24 15:40:58 +0100

    Player movement added

---
+++ Source/WarehouseSim/PlayerFPCharacter.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerFPCharacter.h"
+
+// Sets default values
+APlayerFPCharacter::APlayerFPCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	bUseControllerRotationYaw = false;
+
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	PlayerCamera->SetupAttachment(RootComponent);
+	PlayerCamera->SetRelativeLocation(FVector(0, 0, 40));
+}
+
+// Called when the game starts or when spawned
+void APlayerFPCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerFPCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerFPCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("Forward", this, &APlayerFPCharacter::MoveForward);
+	InputComponent->BindAxis("Right", this, &APlayerFPCharacter::MoveRight);
+	InputComponent->BindAxis("LookRight", this, &APlayerFPCharacter::Turn);
+	InputComponent->BindAxis("LookUP", this, &APlayerFPCharacter::LookUp);
+	
+}
+
+void APlayerFPCharacter::MoveForward(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorForwardVector(), value);
+	}
+}
+void APlayerFPCharacter::MoveRight(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorRightVector(), value);
+	}
+}
+void APlayerFPCharacter::Turn(float value)
+{
+	if (value)
+	{
+		AddActorLocalRotation(FRotator(0, value, 0));
+	}
+}
+void APlayerFPCharacter::LookUp(float value)
+{
+	if (value)
+	{
+		float temp = PlayerCamera->GetRelativeRotation().Pitch + value;
+
+		if (temp < 65 && temp > -65)
+		{
+			PlayerCamera->AddLocalRotation(FRotator(value, 0, 0));
+		}
+	} 
+}
---
+++ Source/WarehouseSim/PlayerFPCharacter.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Components/ActorComponent.h"
+#include "Camera/CameraComponent.h"
+#include "PlayerFPCharacter.generated.h"
+
+UCLASS()
+class WAREHOUSESIM_API APlayerFPCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerFPCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void Turn(float Value);
+	void LookUp(float Value);
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+		UCameraComponent* PlayerCamera;
+	
+};
